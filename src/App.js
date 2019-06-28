3596c9f02ab5c1cde5bcee8bcb6d3cb2c06646bc
xlisachan <xlisachan@gmail.com>
2019-06-28 15:21:28 -0400

    Revised App.js - updated and added functions to save date (created, displayed in header and list items)

--- src/App.js
+++ src/App.js
@@ -7,41 +7,65 @@ class App extends Component {
     state = data
 
     componentDidMount(){
-      const weekdays = {
-        '0': 'Sunday',
-        '1': 'Monday',
-        '2': 'Tuesday',
-        '3': 'Wednesday',
-        '4': 'Thursday',
-        '5': 'Friday',
-        '6': 'Saturday',
-        '7': 'Sunday'
-      }
-
-      const months = {
-        '1': 'January',
-        '2': 'February',
-        '3': 'March',
-        '4': 'April',
-        '5': 'May',
-        '6': 'June',
-        '7': 'July',
-        '8': 'August',
-        '9': 'September',
-        '10': 'October',
-        '11': 'November',
-        '12': 'December'
-      }
-
-      const newDate = new Date();
-      const weekday = newDate.getDay();
-      const month = newDate.getMonth() + 1;
-      const day = newDate.getDate();
-      const date = weekdays[weekday] + ', ' + months[month] + ' ' + day;
-
       this.setState({
-          todays_date: date
+          todays_date: this.getHeaderDate(this.getDate())
       });
+    }
+
+    getDate = () => {
+        const weekdays = {
+          '0': 'Sunday',
+          '1': 'Monday',
+          '2': 'Tuesday',
+          '3': 'Wednesday',
+          '4': 'Thursday',
+          '5': 'Friday',
+          '6': 'Saturday',
+          '7': 'Sunday'
+        }
+
+        const months = {
+          '0': 'January',
+          '1': 'February',
+          '2': 'March',
+          '3': 'April',
+          '4': 'May',
+          '5': 'June',
+          '6': 'July',
+          '7': 'August',
+          '8': 'September',
+          '9': 'October',
+          '10': 'November',
+          '11': 'December'
+        }
+
+        const newDate = new Date();
+        const weekday = newDate.getDay();
+        const month = newDate.getMonth();
+        const day = newDate.getDate();
+        const year = newDate.getFullYear();
+
+        const date = {
+            newDate: newDate,
+            weekday: weekdays[weekday],
+            month: months[month],
+            day: day,
+            year: year
+        }
+
+        return date;
+    }
+
+    getHeaderDate = date => {
+      return date.weekday + ', ' + date.month + ' ' + date.day;
+    }
+
+    getDateCreated = date => {
+      return date.newDate;
+    }
+
+    getListItemDate = date => {
+      return date.weekday.substr(0,3) + ', ' + date.month.substr(0,3) + ' ' + date.day;
     }
 
     toggleCompleted = id => {
@@ -82,12 +106,13 @@ class App extends Component {
             "list": "Tasks",
             "completed": false,
             "moreInfo": false,
-            "date_created": "",
+            "date_created": this.getDateCreated(this.getDate()),
             "date_due": "",
+            "date_display": this.getListItemDate(this.getDate()),
             "my_day": true,
             "planned": false,
             "important": false,
-            "tasks": false
+            "tasks": true
           }
         ]
       }))
