85186de12a12e6fd964a7bc3e855128c31b14837
xlisachan <xlisachan@gmail.com>
2019-07-07 17:05:44 -0400

    Refactored actions.js

--- src/actions.js
+++ src/actions.js
@@ -1,32 +1,24 @@
 import C from './constants';
 
-export const getListTitle = title => dispatch => {
-    dispatch({
-        type: C.SET_TITLE,
-        payload: title
-    })
-}
+export const getListTitle = title => ({
+    type: C.SET_TITLE,
+    payload: title
+})
 
-export const toggleCompleted = id => dispatch => {
-    dispatch({
-        type: C.EDIT_TASK_STATUS_COMPLETE,
-        payload: id
-    })
-}
+export const toggleCompleted = id => ({
+    type: C.EDIT_TASK_STATUS_COMPLETE,
+    payload: id
+})
 
-export const toggleImportant = id => dispatch => {
-    dispatch({
-        type: C.EDIT_TASK_STATUS_IMPORTANT,
-        payload: id
-    })
-}
+export const toggleImportant = id => ({
+    type: C.EDIT_TASK_STATUS_IMPORTANT,
+    payload: id
+})
 
-export const toggleMoreInfo = task => dispatch => {
-    dispatch({
-        type: C.SET_MOREINFO,
-        payload: task
-    })
-}
+export const toggleMoreInfo = task => ({
+    type: C.SET_MOREINFO,
+    payload: task
+})
 
 export const addTask = (
         task_id, item, list, completedStatus=false, importantStatus=false, moreInfo=[],
