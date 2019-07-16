9c16006e5c1e85e1305d0fb2216098525fddd3fe
xlisachan <xlisachan@gmail.com>
2019-07-16 16:25:14 -0400

    Created NoMatch component and updated stylesheet

---
+++ src/components/Main/NoMatch.js
@@ -0,0 +1,11 @@
+import React from 'react';
+
+const NoMatch = () => {
+    return (
+        <div className="no-match">
+            <p>We searched high and low but couldn't find what you're looking for.</p>
+        </div>
+    );
+}
+ 
+export default NoMatch;
