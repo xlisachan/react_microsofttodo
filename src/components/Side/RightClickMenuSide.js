9781ddc01e0717f697f151a5e2eda73380af8f3a
xlisachan <xlisachan@gmail.com>
2019-08-07 16:43:05 -0400

    Revised RightClickMenuSide component - added menu item to rename todo list

--- src/components/Side/RightClickMenuSide.js
+++ src/components/Side/RightClickMenuSide.js
@@ -1,10 +1,18 @@
 import React from 'react';
 import PropTypes from 'prop-types';
 import { ContextMenu, MenuItem, ContextMenuTrigger } from 'react-contextmenu';
+import { FaRegEdit } from 'react-icons/fa';
 import TitleRow from '../../containers/Side/TitleRow';
 import DeleteModal from '../DeleteModal';
 
-const RightClickMenuSide = ({list, tasks, onClick=f=>f, onRemoveList=f=>f, onRemoveTask=f=>f}) => {
+const RightClickMenuSide = ({list, tasks, onClick=f=>f, onEditClick=f=>f, onRemoveList=f=>f, onRemoveTask=f=>f}) => {
+
+    const renderRenameList = () =>
+        <MenuItem onClick={() => onEditClick()}>
+            <FaRegEdit style={{margin: '0 5px 3px 0'}} />
+            <span>Rename list</span>
+        </MenuItem>
+    
     const removeTasks = () => {
         const listTasks = tasks.filter(task => task.list_id === list.id);
 
@@ -38,7 +46,9 @@ const RightClickMenuSide = ({list, tasks, onClick=f=>f, onRemoveList=f=>f, onRem
             </ContextMenuTrigger>
 
             <ContextMenu id={list.id}>
-               { renderRemoveList() }
+                { renderRenameList() }
+
+                { renderRemoveList() }
             </ContextMenu>
         </div>
     );
@@ -47,6 +57,7 @@ const RightClickMenuSide = ({list, tasks, onClick=f=>f, onRemoveList=f=>f, onRem
 RightClickMenuSide.propTypes = {
     list: PropTypes.any,
     onClick: PropTypes.func.isRequired,
+    onEditClick: PropTypes.func.isRequired,
     tasks: PropTypes.array.isRequired,
     onRemoveList: PropTypes.func.isRequired,
     onRemoveTask: PropTypes.func.isRequired
