7572fd5c97917720c574a47c565673fae8b93289
xlisachan <xlisachan@gmail.com>
2019-08-13 17:45:39 -0400

    Revised MoreInfo component - added TopSection, updated onClick listener

--- src/components/MoreInfo/MoreInfo.js
+++ src/components/MoreInfo/MoreInfo.js
@@ -3,10 +3,12 @@ import PropTypes from 'prop-types';
 import { makeStyles, useTheme } from '@material-ui/core/styles';
 import { Divider, Drawer, Hidden, IconButton } from '@material-ui/core';
 import { FaChevronLeft, FaChevronRight } from 'react-icons/fa';
+import TopSection from '../../containers/MoreInfo/TopSection';
 
 const drawerWidth = 250;
 
 const useStyles = makeStyles(theme => ({
+    toolbar: theme.mixins.toolbar,
     drawer: {
         width: drawerWidth,
         flexShrink: 0,
@@ -17,49 +19,47 @@ const useStyles = makeStyles(theme => ({
     },
     drawerHeader: {
         display: 'flex',
-        alignItems: 'center',
+        flexDirection: 'column',
         padding: '0 8px',
         ...theme.mixins.toolbar,
         justifyContent: 'flex-start',
     },
 }));
 
-const MoreInfo = ({open, onClick=f=>f}) => {
+const MoreInfo = ({toggleMore, onCloseMore=f=>f}) => {
     const classes = useStyles();
     const theme = useTheme();
 
-    const renderMore = () => {
-        return (
-            <Hidden smDown implementation="css">
-                <Drawer
-                    className={classes.drawer}
-                    variant="persistent"
-                    anchor="right"
-                    open={open}
-                    classes={{
-                        paper: classes.drawerPaper,
-                    }}
-                >
-                    <div className={classes.drawerHeader}>
-                        <Divider />
-
-                        <IconButton onClick={onClick}>
-                            {theme.direction === 'rtl' ? <FaChevronLeft /> : <FaChevronRight />}
-                        </IconButton>
-                    </div>
-                </Drawer>
-            </Hidden>
-        )
-    }
-
     return (
-        renderMore()
+        <Hidden smDown implementation="css">
+            <Drawer
+                className={classes.drawer}
+                variant="persistent"
+                anchor="right"
+                open={toggleMore}
+                classes={{
+                    paper: classes.drawerPaper,
+                }}>
+
+                <div className={classes.drawerHeader}>
+                    <div className={classes.toolbar} />
+
+                    <TopSection />
+
+                    <Divider />
+
+                    <IconButton onClick={() => onCloseMore() }>
+                        {theme.direction === 'rtl' ? <FaChevronLeft /> : <FaChevronRight />}
+                    </IconButton>
+                </div>
+            </Drawer>
+        </Hidden>
     )
 }
 
 MoreInfo.propTypes = {
-    open: PropTypes.bool.isRequired,
-    onClick: PropTypes.func.isRequired
+    toggleMore: PropTypes.bool.isRequired,
+    onCloseMore: PropTypes.func.isRequired
 }
 
 export default MoreInfo;
