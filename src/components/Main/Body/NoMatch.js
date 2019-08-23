2282e66d2745cd2dfdec6e2b29f9a37cf5350541
xlisachan <xlisachan@gmail.com>
2019-08-23 12:57:18 -0400

    Revised component per updated stylesheet

--- src/components/Main/Body/NoMatch.js
+++ src/components/Main/Body/NoMatch.js
@@ -1,7 +1,7 @@
 import React from 'react';
 
 const NoMatch = () =>
-    <div className="no-match">
+    <div className="no-match align-center">
         <p>We searched high and low but couldn't find what you're looking for.</p>
     </div>
  
