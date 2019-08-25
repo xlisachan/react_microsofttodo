f209eeb6beb55fa22f797a72006b2f7811a01c32
xlisachan <xlisachan@gmail.com>
2019-08-25 14:00:52 -0400

    Revised Sidebar component - added Search component

--- src/components/Side/Sidebar.js
+++ src/components/Side/Sidebar.js
@@ -1,48 +1,11 @@
 import React, { useState } from 'react';
 import PropTypes from 'prop-types';
-import { FaSearch, FaTimesCircle } from 'react-icons/fa';
-import { fade, makeStyles } from '@material-ui/core/styles';
-import InputBase from '@material-ui/core/InputBase';
+import Search from './Search';
 import TitleList from '../../containers/Side/TitleList';
 import AddList from '../../containers/Side/AddList';
 
-const useStyles = makeStyles(theme => ({
-    toolbar: theme.mixins.toolbar,
-    search: {
-        position: 'relative',
-        borderRadius: theme.shape.borderRadius,
-        backgroundColor: fade('#787', 0.15),
-        marginRight: theme.spacing(2),
-        marginLeft: 0,
-        width: '50%',
-        [theme.breakpoints.up('sm')]: {
-            marginLeft: theme.spacing(3),
-            width: 'auto',
-        },
-    },
-    searchIcon: {
-        width: theme.spacing(5),
-        height: '100%',
-        position: 'absolute',
-        pointerEvents: 'none',
-        display: 'flex',
-        alignItems: 'center',
-        justifyContent: 'center',
-    },
-    inputRoot: {
-        color: 'inherit',
-    },
-    inputInput: {
-        padding: theme.spacing(1, 1, 1, 6),
-        transition: theme.transitions.create('width'),
-        width: '90%',
-    },
-}));
-
 const Sidebar = ({onChangeQuery=f=>f, onClear=f=>f, onClose=f=>f, onEditClick=f=>f}) => {
-    const classes = useStyles();
     const [searchItem, setSearchItem] = useState('');
-
     let _searchInput;
 
     const handleChange = e => {
@@ -58,29 +21,7 @@ const Sidebar = ({onChangeQuery=f=>f, onClear=f=>f, onClose=f=>f, onEditClick=f=
 
     return (
         <div>
-            <div className={classes.toolbar} />
-
-            <div className={classes.search} style={{width: '90%', margin: 'auto'}}>
-                <div className={classes.searchIcon}>
-                    <FaSearch style={{fontSize: '1rem'}} />
-                </div>
-
-                <InputBase
-                    style={{width: '88%'}}
-                    placeholder="Search"
-                    classes={{
-                        root: classes.inputRoot,
-                        input: classes.inputInput,
-                    }}
-                    inputProps={{ 'aria-label': 'Search' }}
-                    inputRef={ input => _searchInput = input }
-                    onChange={ handleChange }
-                />
-
-                { !searchItem ? 
-                    null : <FaTimesCircle style={{fontSize: '1rem'}} onClick={ clearSearch } />
-                }
-            </div>
+            <Search ref={ input=> _searchInput = input} searchItem={ searchItem } onChangeQuery={ handleChange } onClear={ clearSearch } />
 
             <TitleList onClick={ clearSearch } onClose={ onClose } onEditClick={ onEditClick } />
 
@@ -92,6 +33,7 @@ const Sidebar = ({onChangeQuery=f=>f, onClear=f=>f, onClose=f=>f, onEditClick=f=
 Sidebar.propTypes = {
     onChangeQuery: PropTypes.func,
     onClear: PropTypes.func.isRequired,
+    onClose: PropTypes.func.isRequired,
     onEditClick: PropTypes.func
 }
 
