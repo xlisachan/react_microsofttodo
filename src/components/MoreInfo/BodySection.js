0f1d50340f2c42a40341a6384f7d5d88a72a6cbf
xlisachan <xlisachan@gmail.com>
2019-09-02 19:57:10 -0400

    Created BodySection component of MoreInfo section

---
+++ src/components/MoreInfo/BodySection.js
@@ -0,0 +1,37 @@
+import React from 'react';
+import PropTypes from 'prop-types';
+import { Divider } from '@material-ui/core';
+import Steps from './Steps/Steps';
+import MoreMyDay from '../../containers/MoreInfo/MyDay/MoreMyDay';
+import MorePlanned from './Planned/MorePlanned';
+import Note from '../../containers/MoreInfo/Note/Note';
+
+const BodySection = ({selectedTask, tasks, onClose}) => {
+    return (
+        <div style={{position: 'relative', top: 100}}>
+            <div style={{height: '82vh', overflow: 'auto'}}>
+                <Steps selectedTask={ selectedTask } />
+
+                <Divider />
+
+                <MoreMyDay selectedTask={ selectedTask } onClose={ onClose } />
+
+                <Divider />
+
+                <MorePlanned selectedTask={ selectedTask } tasks={ tasks } />
+
+                <Divider />
+
+                <Note selectedTask={ selectedTask } />
+            </div>
+        </div>
+    );
+}
+
+BodySection.propTypes = {
+    selectedTask: PropTypes.array.isRequired,
+    tasks: PropTypes.array.isRequired,
+    onClose: PropTypes.func.isRequired
+}
+
+export default BodySection;
