955c853fb84439a307cc47423a5298c22806d214
xlisachan <xlisachan@gmail.com>
2019-11-03 16:05:24 -0500

    Revised Main component - added ResultsContainer and TodoList components

--- src/components/Main/Main.js
+++ src/components/Main/Main.js
@@ -1,12 +1,35 @@
+import React from 'react';
 import PropTypes from 'prop-types';
+import ResultsContainer from './Body/ResultsContainer';
+import TodoList from './Body/TodoList';
 
-const Main = ({query, renderMain}) => {
-    return query ? renderMain.searchResults() : renderMain.todoList()
-}
+const Main = React.forwardRef(({filteredTasks, open, query, selectedList, onOpen=f=>f, onClose=f=>f}, ref) => {
+    return query ? 
+        <ResultsContainer
+            filteredTasks={filteredTasks}
+            open={open}
+            selectedList={selectedList}
+            onOpen={onOpen}
+            onClose={onClose}
+        /> 
+        : 
+        <TodoList
+            ref={ref}
+            filteredTasks={filteredTasks}
+            open={open}
+            selectedList={selectedList}
+            onOpen={onOpen}
+            onClose={onClose}
+        />
+})
 
 Main.propTypes = {
+    filteredTasks: PropTypes.array,
+    open: PropTypes.bool.isRequired,
     query: PropTypes.string.isRequired,
-    renderMain: PropTypes.object.isRequired
+    selectedList: PropTypes.array,
+    onOpen: PropTypes.func.isRequired,
+    onClose: PropTypes.func.isRequired
 }
 
 export default Main;
