322a51b7482e90d98dd4b412a15c0dd4145d88f2
xlisachan <xlisachan@gmail.com>
2020-03-24 18:01:58 -0400

    Updates

---
+++ precache-manifest.beca63c65af0aa88f27526d1cae818a6.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "e5c2d151b19741dbc1af78a04b293b15",
+    "url": "/react_microsofttodo/index.html"
+  },
+  {
+    "revision": "a46b4a6662b4be1be594",
+    "url": "/react_microsofttodo/static/css/main.6eaf212c.chunk.css"
+  },
+  {
+    "revision": "1086a1034de5b915e296",
+    "url": "/react_microsofttodo/static/js/2.7fb42a93.chunk.js"
+  },
+  {
+    "revision": "2e3bdcfcbb1aa2be0c8874452de30a76",
+    "url": "/react_microsofttodo/static/js/2.7fb42a93.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "a46b4a6662b4be1be594",
+    "url": "/react_microsofttodo/static/js/main.699b6542.chunk.js"
+  },
+  {
+    "revision": "7c77f3cb1e34271b0c27",
+    "url": "/react_microsofttodo/static/js/runtime-main.abe5697b.js"
+  }
+]);
